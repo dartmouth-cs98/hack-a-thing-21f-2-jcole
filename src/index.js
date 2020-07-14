1a911dd85e1ea0ebb05eee02e02bf1f301745a30
Jared Cole <jcole.ny2000@gmail.com>
2020-07-14 15:18:15 -0400

    mostly done now

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('Starting up');
+import $ from 'jquery';
+import './style.scss';
+
+// $('#main').html('Here we go!');
+let num = 0;
+setInterval(() => {
+  $('#main').html(`You've been on this page for ${num} seconds.`);
+  num += 1;
+}, 1000);
